406a7c2621ce2bce99659af2f47414da85b9a33e
falqassab <fatoomynana@gmail.com>
2025-10-26 17:03:04 +0300

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -31,7 +31,7 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on',
+    trace: 'retain-on-failure',
     slowMo: 500,
     video: 'on', 
   },
